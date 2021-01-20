84588cd0794c75443ccbb3b96e56b59ac7c9e362
educalok <eduardo.calzone@gmail.com>
2021-01-20 13:24:26 -0300

    .

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const { ApolloServer , PubSub} = require('apollo-server')
-const mongoose = require('mongoose')
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config')
-const typeDefs = require('./graphql/typeDefs')
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
-const pubSub = new PubSub()
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req, pubSub }), 
-})
+  context: ({ req }) => ({ req, pubsub }),
+});
 
-mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
-  console.log('Se conecto');
-  return server.listen({ port: 5000 })
-})
-  .then(res => { console.log(`Server running at ${res.url}`); })
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
