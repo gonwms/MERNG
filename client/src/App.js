92960f0bc8b63c03d1babe278b6026eb7796eda5
educalok <eduardo.calzone@gmail.com>
2021-01-21 23:09:56 -0300

    Frontend 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
     <div>
-      <h1>Hi world</h1>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Container>
+      </Router>
     </div>
   );
 }
